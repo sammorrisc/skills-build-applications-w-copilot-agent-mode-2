5f6acfff65fbd8b5a26dcb22cb9690c2a230f928
sammorrisc <sammorris207@gmail.com>
2026-02-15 18:39:32 +0000

    Add initial Django app structure with models, views, serializers, and settings

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel')
+        self.assertEqual(user.email, 'test@hero.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='Marvel')
+        self.assertEqual(team.name, 'Marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel')
+        activity = Activity.objects.create(user=user, type='Running', duration=30, date='2026-02-15')
+        self.assertEqual(activity.type, 'Running')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do 20 pushups', suggested_for='Marvel')
+        self.assertEqual(workout.name, 'Pushups')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        team = Team.objects.create(name='Marvel')
+        leaderboard = Leaderboard.objects.create(team=team, points=100, rank=1)
+        self.assertEqual(leaderboard.rank, 1)
